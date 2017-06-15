5fd50faf2fd1cd39fec6ca9cac0e9f9ea982f77c
sbese <sergbuks2@gmail.com>
2017-06-15 18:55:29 +0300

    fix bot.py
    fix bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,41 @@
 import telebot
 
 class Bot:
+    bot=0
+    client_chat_id=280629735
     def __init__(self,token):
         self.bot=telebot.TeleBot(token)
 
-    def update(message):
+        @self.bot.message_handler()
+        def nm(message):
+            global to_id
+            global p
+            p = message
+            if message.chat.id==self.client_chat_id:
+                if message.text.find('forward_to_')!=-1:
+                    to_id=int(message.text.partition('forward_to_')[2])
+
+                else: self.bot.forward_message(chat_id=to_id,
+                                          from_chat_id=self.client_chat_id,
+                                          message_id=message.message_id)
+
+            else:
+                splited_text=message.text.split()
+                if splited_text[0]=='/update':
+                    self.update(message)
+                if splited_text[0]=='/add':
+                    pass
+                if splited_text[0]=='/dell':
+                    pass
+
+    def update_feed(message):
         pass
-    def add(message):
+    def add_channels(message):
         pass
-    def del(message):
+    def del_channels(message):
         pass
     
-    @bot.message_handler()
-    def nm(message):
-        global to_id
-        global p
-        p = message
-        if message.chat.id==client_chat_id:
-            if message.text.find('forward_to_')!=-1:
-                to_id=int(message.text.partition('forward_to_')[2])
-
-            else: bot.forward_message(chat_id=to_id,
-                                      from_chat_id=client_chat_id,
-                                      message_id=message.message_id)
-
-        else:
-            splited_text=message.text.split()
-            if splited_text[0]=='/update':
-                self.update(message)
-            if splited_text[0]=='/add':
-                pass
-            if splited_text[0]=='/dell':
-                pass
+   
 
     def start(self):
         self.bot.polling(none_stop=True)
