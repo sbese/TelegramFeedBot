bd53612386f72dd925aba760b3ac8558e5cb45b1
NikelWolf <snive2013@yandex.ru>
2017-06-16 19:22:54 +0300

    join channel method added

--- client.py
+++ client.py
@@ -74,8 +74,9 @@ class Client:
         """
 
         return self.get_InputPeer_by_enity(
-                self.client.invoke(
-                    self.TG.contacts.ResolveUsernameRequest(username))
+                    self.client.invoke(
+                        self.TG.contacts.ResolveUsernameRequest(username)
+                    )
                 )
 
     def get_messages_enity_by_enity(self, enity, messages_count=50):
@@ -88,15 +89,16 @@ class Client:
         """
 
         return self.client.invoke(
-            self.TG.messages.GetHistoryRequest(
-                peer = self.get_InputPeer_by_enity(enity),
-                offset_id = 0,
-                offset_date = None,
-                add_offset = 0,
-                limit = messages_count,
-                max_id = 0,
-                min_id = 0)
-            )
+                    self.TG.messages.GetHistoryRequest(
+                        peer = self.get_InputPeer_by_enity(enity),
+                        offset_id = 0,
+                        offset_date = None,
+                        add_offset = 0,
+                        limit = messages_count,
+                        max_id = 0,
+                        min_id = 0
+                    )
+                )
 
     def get_messages_enity_by_username(self, username, messages_count=50):
         """
@@ -108,15 +110,16 @@ class Client:
         """
 
         return self.client.invoke(
-            self.TG.messages.GetHistoryRequest(
-                peer = self.get_InputPeer_by_username(username),
-                offset_id = 0,
-                offset_date = None,
-                add_offset = 0,
-                limit = messages_count,
-                max_id = 0,
-                min_id = 0)
-            )
+                    self.TG.messages.GetHistoryRequest(
+                        peer = self.get_InputPeer_by_username(username),
+                        offset_id = 0,
+                        offset_date = None,
+                        add_offset = 0,
+                        limit = messages_count,
+                        max_id = 0,
+                        min_id = 0
+                    )
+                )
 
     def forward_messages_by_enities(self, from_enity, to_enity, messages_enity):
         """
@@ -132,13 +135,14 @@ class Client:
         """
 
         return self.client.invoke(
-            self.TG.messages.ForwardMessagesRequest(
-                from_peer = self.get_InputPeer_by_enity(from_enity),
-                id = [msg.id for msg in messages_enity.messages],
-                to_peer = self.get_InputPeer_by_enity(to_enity),
-                random_id = [self.tl.helpers.generate_random_long() for _ in
-                            range(len(messages_enity.messages))])
-            )
+                    self.TG.messages.ForwardMessagesRequest(
+                        from_peer = self.get_InputPeer_by_enity(from_enity),
+                        id = [msg.id for msg in messages_enity.messages],
+                        to_peer = self.get_InputPeer_by_enity(to_enity),
+                        random_id = [self.tl.helpers.generate_random_long() for _ in
+                                    range(len(messages_enity.messages))]
+                    )
+                )
 
     def forward_messages_by_peernames(self, from_peer_name, to_peer_name, messages_enity):
         """
@@ -150,13 +154,14 @@ class Client:
         """
 
         return self.client.invoke(
-            self.TG.messages.ForwardMessagesRequest(
-                from_peer = self.get_InputPeer_by_username(from_peer_name),
-                id = [msg.id for msg in messages_enity.messages],
-                to_peer = self.get_InputPeer_by_username(to_peer_name),
-                random_id = [self.tl.helpers.generate_random_long() for _ in
-                            range(len(messages_enity.messages))])
-            )
+                    self.TG.messages.ForwardMessagesRequest(
+                        from_peer = self.get_InputPeer_by_username(from_peer_name),
+                        id = [msg.id for msg in messages_enity.messages],
+                        to_peer = self.get_InputPeer_by_username(to_peer_name),
+                        random_id = [self.tl.helpers.generate_random_long() for _ in
+                                    range(len(messages_enity.messages))]
+                    )
+                )
 
     def forward_messages_by_InputPeers(self, from_peer, to_peer, messages_enity):
         """
@@ -170,10 +175,23 @@ class Client:
         """
 
         return self.client.invoke(
-        self.TG.messages.ForwardMessagesRequest(
-            from_peer = from_peer,
-            id = [msg.id for msg in messages_enity.messages],
-            to_peer = to_peer,
-            random_id = [self.tl.helpers.generate_random_long() for _ in
-                        range(len(messages_enity.messages))])
-        )
+                    self.TG.messages.ForwardMessagesRequest(
+                        from_peer = from_peer,
+                        id = [msg.id for msg in messages_enity.messages],
+                        to_peer = to_peer,
+                        random_id = [self.tl.helpers.generate_random_long() for _ in
+                                    range(len(messages_enity.messages))]
+                    )
+                )
+
+    def join_channel_by_channelname(self, channelname):
+        """
+        Join this client to channel with name given as parametr
+
+        channelname - string name of channel that must be added to user
+        """
+
+        self.client.invoke(
+                self.TG.channels.JoinChannelRequest(
+                    self.get_InputPeer_by_username(channelname))
+            )
