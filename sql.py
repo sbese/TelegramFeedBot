585b455b8bcd2fe192eedd18771de871081482c5
sbese <sergbuks2@gmail.com>
2017-06-17 09:49:25 +0300

    yield!!!!

--- sql.py
+++ sql.py
@@ -18,18 +18,37 @@ class SQL_helper:
         ids=[x[0] for x in [x for x in self.cursor.execute('SELECT id FROM {}'.format(table))]]
         print(ids)
         if not (chat_id in ids):
-            self.cursor.execute('INSERT INTO {}(id) VALUES({})'.format(table,str(chat_id)))
+            self.cursor.execute(
+                'INSERT INTO {}(id) VALUES({})'.format(
+                    table,str(chat_id)
+                    )
+                )
             self.conn.commit()
 
     def get_channels(self,chat_id,table):
 
-        return[x[0] for x in [x for x in self.cursor.execute('select channels from test1 where id=2')]][0].split(',')
+        return[
+            x[0] for x in [
+                x for x in self.cursor.execute(
+                    'select channels from {} where id={}'.format(
+                        table,str(chat_id)
+                        )
+                    )
+                ]
+            ][0].split(',')
 
-    def check_channel(self,list_channels,table):
+    def missing_channels(self,list_channels,table):
 
-        channels=[x[0] for x in [x for x in self.cursor.execute('SELECT channel FROM {}'.format(table))]]
+        channels=[
+            x[0] for x in [
+                x for x in self.cursor.execute(
+                    'SELECT channel FROM {}'.format(table)
+                    )
+                ]
+            ]
 
         for channel in list_channels:
             if not (channel in channels):
-                self.cursor.execute('INSERT INTO{}(channel) VALUES({})'.format(table,channel))
+                yield channel
+
 
